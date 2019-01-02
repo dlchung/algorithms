ce327dd93ce59ed66a6dfa660caec33f797345e5
Dan Chung <danetix@gmail.com>
2019-01-02 13:10:26 -0500

    added previous solution

--- codewars/6kyu_multiples_of_3_or_5.rb
+++ codewars/6kyu_multiples_of_3_or_5.rb
@@ -1,6 +1,18 @@
 # https://www.codewars.com/kata/514b92a657cdc65150000006
 
+# def solution(number)
+#   # put your solution here
+#   sum = 0
+#   while number > 0
+#     number -= 1
+#     if number % 3 == 0 || number % 5 == 0
+#       sum += number
+#     end
+#   end
+#   sum
+# end
+
 def solution(number)
   # put your solution here
   (1...number).select { |num| num % 3 == 0 || num % 5 == 0 }.sum
- end
+end
