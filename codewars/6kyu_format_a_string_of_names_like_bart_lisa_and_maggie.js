c8d30ca56bc5b81d4c74b5a64f1dc9771cc29bf6
Dan Chung <danetix@gmail.com>
2019-01-02 13:07:52 -0500

    refactored

--- codewars/6kyu_format_a_string_of_names_like_bart_lisa_and_maggie.js
+++ codewars/6kyu_format_a_string_of_names_like_bart_lisa_and_maggie.js
@@ -1,16 +1,28 @@
 // https://www.codewars.com/kata/53368a47e38700bd8300030d
 
-function list(names){
-  let nameList = ""
+// function list(names){
+//   let nameList = ""
   
-  for(let i = 0; i < names.length; i++) {
-    nameList += names[i].name
-    if(names.length - i === 2) {
-      nameList += " & "
-    } 
-    if(names.length - i > 2) {
-      nameList += ", "
-    }
-  }  
-  return nameList
+//   for(let i = 0; i < names.length; i++) {
+//     nameList += names[i].name
+//     if(names.length - i === 2) {
+//       nameList += " & "
+//     } 
+//     if(names.length - i > 2) {
+//       nameList += ", "
+//     }
+//   }  
+//   return nameList
+// }
+
+function list(names){
+  //your code here
+  let phrase = ""
+  names.forEach((name, index) => {
+    phrase += name.name
+    index >= 0 && index < names.length - 2 ? phrase += ", " : null
+    index === names.length - 2 ? phrase += " & " : null
+  })
+  
+  return phrase
 }
