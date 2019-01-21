f0daee9e8e741822c9d13935077cbd99ae24e506
Dan Chung <danetix@gmail.com>
2019-01-21 15:47:41 -0500

    refactor

--- codewars/7kyu_shortest_word.py
+++ codewars/7kyu_shortest_word.py
@@ -1,11 +1,16 @@
 # https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
 
+# def find_short(s):
+#     # your code here
+#     last = ""
+    
+#     for word in s.split():
+#         if len(word) < len(last) or last == "":
+#             last = word
+    
+#     return len(last) # l: shortest word length
+
 def find_short(s):
     # your code here
-    last = ""
-    
-    for word in s.split():
-        if len(word) < len(last) or last == "":
-            last = word
-    
-    return len(last) # l: shortest word length
+    result = map(lambda word: len(word), s.split())
+    return min(result)
