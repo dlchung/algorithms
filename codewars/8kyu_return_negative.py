7b47bd3bf66dba37f4865970c8023ec734d90310
Dan Chung <danetix@gmail.com>
2019-01-21 14:49:44 -0500

    refactor

--- codewars/8kyu_return_negative.py
+++ codewars/8kyu_return_negative.py
@@ -1,6 +1,9 @@
 # https://www.codewars.com/kata/55685cd7ad70877c23000102
 
+# def make_negative( number ):
+#     if number > 0:
+#         return number * -1
+#     return number
+
 def make_negative( number ):
-    if number > 0:
-        return number * -1
-    return number
+    return -abs(number)
