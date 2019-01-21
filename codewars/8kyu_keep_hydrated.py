d8b73440b42aebf973b3476e554ef2ddefad4235
Dan Chung <danetix@gmail.com>
2019-01-21 16:03:43 -0500

    refactor

--- codewars/8kyu_keep_hydrated.py
+++ codewars/8kyu_keep_hydrated.py
@@ -1,5 +1,8 @@
 # https://www.codewars.com/kata/582cb0224e56e068d800003c
 
-import math
+# import math
+# def litres(time):
+#     return math.floor(time * 0.5)
+
 def litres(time):
-    return math.floor(time * 0.5)
+    return time // 2 # double forward slash does integer division
